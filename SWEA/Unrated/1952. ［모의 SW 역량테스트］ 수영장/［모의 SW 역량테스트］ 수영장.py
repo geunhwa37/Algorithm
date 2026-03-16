bd4fa39a6912e4bedaa6ce4d1f96aaea3c941800
이근화 <jacika37@naver.com>
2026-03-16 16:19:02 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 222 ms, Memory: 65,152 KB -BaekjoonHub

---
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -0,0 +1,33 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    # 1일 이용권의 요금, 1달 이용권의 요금, 3달 이용권의 요금, 1년 이용권의 요금
+    day, mon1, mon3, year = map(int, input().split())
+
+    plan = [0] + list(map(int, input().split())) # 12개월 이용 계획
+
+    # 1. 전체 경우를 다보자 -> 완탐
+    # 2. 각달의 최소값 -> DP
+
+    ans = float('inf')
+    # depth : 12 (12월을 모두 고려한 경우)
+    # branch : 3개 (1일, 1달, 3달)
+    def rec(month, total_cost):
+        global ans
+
+        if month >= 13:
+            ans = min(ans, total_cost)
+            return
+
+        # 1일권
+        rec(month + 1, total_cost + (day * plan[month]))
+        # 1달권
+        rec(month + 1, total_cost + mon1)
+        # 3달권
+        rec(month + 3, total_cost + mon3)
+        # 1년권
+        rec(month + 12, total_cost + year)
+
+    rec(1, 0)
+
+    print(f'#{tc} {ans}')
