67823bdf85c12733999eb9abeaceefec777e6c90
이근화 <jacika37@naver.com>
2026-03-31 16:17:08 +0900

    [Gold III] Title: 줄 세우기, Time: 2572 ms, Memory: 40004 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,18 +3,20 @@ from collections import deque
 N, M = map(int, input().split()) # 학생 수, 키를 비교한 횟수
 
 graph = [[] for _ in range(N+1)]
-cnt = [float('inf')] + [0] * N
+cnt = [0] * (N+1)
 
+# 그래프 + 진입차수 세팅
 for _ in range(M):
     A, B = map(int, input().split())
     graph[A].append(B)
     cnt[B] += 1
 
-# 앞 사람 없는 사람부터 시작
+# 큐 초기화 (진입차수 0) - 앞 사람 없는 사람부터 시작
 queue = deque()
 for i in range(1, N+1):
     if cnt[i] == 0: queue.append(i)
 
+# 위상정렬
 while queue:
     q = queue.popleft()
     print(q, end=' ')
